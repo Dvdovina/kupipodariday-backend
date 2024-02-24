0ed51c464a751b0ae6b83672b6afbc1ede752194
Daria Vdovina <vdovinadn@gmail.com>
2024-02-24 22:36:08 +0300

    build: bootstrap, dto skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const { PORT = 3000 } = process.env;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT);
   console.log('Сервер запущен на порту:', 3000); // eslint-disable-line
 }
