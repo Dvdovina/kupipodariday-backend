ed20384d13cbbfd4470072666d93899b231e8cea
Daria Vdovina <vdovinadn@gmail.com>
2024-03-03 06:41:11 +0300

    build: users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Body,
+    Patch,
+    UseGuards,
+    Req,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    async findMe(@Req() req): Promise<User> {
+        return await this.usersService.findById(req.user.id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('me')
+    async updateOne(@Req() req, @Body() updateUsertDto: UpdateUserDto) {
+        return await this.usersService.updateOne(req.user.id, updateUsertDto);
+    }
+}
