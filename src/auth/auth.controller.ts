30686603c98d84c044c8bf8d7fb93c8e93fe1756
Daria Vdovina <vdovinadn@gmail.com>
2024-03-03 06:20:12 +0300

    build: AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Req, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly usersService: UsersService,
+    ) { }
+    @UseGuards(AuthGuard('local'))
+    @Post('signin')
+    async signin(@Req() req) {
+        return this.authService.signin(req.user);
+    }
+
+    @Post('signup')
+    async signup(@Body() createUserDto: CreateUserDto) {
+        const user = await this.usersService.create(createUserDto);
+        return this.authService.signin(user);
+    }
+}
