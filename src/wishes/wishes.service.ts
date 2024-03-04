3536a59b0f4b5a63262544333ee424a6e3102e65
Daria Vdovina <vdovinadn@gmail.com>
2024-03-04 19:26:30 +0300

    build: more wishes methods/controllers

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -33,6 +33,20 @@ export class WishesService {
         return await this.wishesRepository.find();
     }
 
+    async findLatestWishes(): Promise<Wish[]> {
+        return await this.wishesRepository.find({
+            take: 40,
+            order: { createdAt: 'DESC' },
+        });
+    }
+
+    async findTopWishes(): Promise<Wish[]> {
+        return await this.wishesRepository.find({
+            take: 20,
+            order: { copied: 'DESC' },
+        });
+    }
+
     async updateOne(
         wishId: number,
         updateWishDto: UpdateWishDto,
@@ -52,6 +66,25 @@ export class WishesService {
             ...updateWishDto,
             updatedAt: new Date(),
         });
+    }
+
+    async copyWish(wishId: number, user: User) {
+        const wish = await this.findOne(wishId);
+        if (!wish) {
+            throw new NotFoundException('Подарок не найден');
+        }
+        if (user.id === wish.owner.id) {
+            throw new BadRequestException('Нельзя копировать свои подарки');
+        }
+        wish.copied += 1;
+        const copy = await this.wishesRepository.create({
+            ...wish,
+            owner: user,
+            copied: 0,
+            id: undefined,
+        });
+        this.wishesRepository.save(wish);
+        return this.wishesRepository.save(copy);
     }
 
     async removeOne(wishId: number, userId: number) {
