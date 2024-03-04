a4c6f9f2f1c50d27dc9dda4cfbb93fa55d46d019
Daria Vdovina <vdovinadn@gmail.com>
2024-03-04 18:44:41 +0300

    build: offers controller&module

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Req,
+    Body,
+    Param,
+    UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { OffersService } from './offers.service';
+import { CreateOfferDto } from './dto/create-offer.dto';
 
 @Controller('offers')
-export class OffersController {}
+export class OffersController {
+    constructor(private readonly offersService: OffersService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    create(@Body() createOfferDto: CreateOfferDto, @Req() req: any) {
+        return this.offersService.create(createOfferDto, req.user.id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    findAll() {
+        return this.offersService.findAll();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.offersService.findOne(id);
+    }
+}
--- src/offers/offers.module.ts
+++ src/offers/offers.module.ts
@@ -3,10 +3,11 @@ import { OffersController } from './offers.controller';
 import { OffersService } from './offers.service';
 import { Offer } from './entities/offer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WishesModule } from 'src/wishes/wishes.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Offer]),
+    TypeOrmModule.forFeature([Offer]), WishesModule,
   ],
   controllers: [OffersController],
   providers: [OffersService],
